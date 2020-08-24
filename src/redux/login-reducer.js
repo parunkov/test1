105409827287836b65493c7e8d7e12ba223dbca6
parunkov <parunkov@gmail.com>
2020-08-24 12:32:25 +0500

    Сделал обработку ошибки

--- src/redux/login-reducer.js
+++ src/redux/login-reducer.js
@@ -2,17 +2,20 @@ import 'isomorphic-fetch';
 import Sendsay from 'sendsay-api';
 
 const LOGINED = 'login/LOGINED';
+const ERROR = 'login/ERROR';
 
 const initialState = {
 	isLogined: false,
 	login: null,
 	sublogin: null,
-	password: null
+	password: null,
+	error: null
 }
 
 const loginReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case LOGINED: {
+		case LOGINED:
+		case ERROR: {
 			return {
 				...state,
 				...action.payload
@@ -27,6 +30,10 @@ const setLogin = (login, sublogin, password) => ({
 	type: LOGINED,
 	payload: {isLogined: true, login, sublogin, password}
 });
+const setError = (error) => ({
+	type: ERROR,
+	payload: {error}
+})
 
 export const checkLogin = (login, sublogin, password) => (dispatch) => {
 	var sendsay = new Sendsay({
@@ -34,9 +41,12 @@ export const checkLogin = (login, sublogin, password) => (dispatch) => {
 	});
 
 	sendsay.request({ action: 'sys.settings.get', list: ['about.id']}).then(function(res) {
-		console.log(res);
+		// console.log(res);
 		dispatch(setLogin(login, sublogin, password));
-	})
+	}).catch(err => {
+		// console.log(err.explain);
+		dispatch(setError(err.explain));
+	});
 }
 
 export default loginReducer;
