3628882fe4b34d7996a6ed8f0cb58bb58da4d1e3
parunkov <parunkov@gmail.com>
2020-08-26 13:32:00 +0500

    Сделал индикацию ошибки

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import './History.scss';
+
+const HistoryItem = ({item}) => {
+	return(
+		<span className="History__item">
+			<span className={item.isError ? "History__error History__error_enabled" : "History__error"}></span>
+			<span className="History__title">{item.title}</span>
+		</span>
+	)
+}
 
 const History = ({history}) => {
-	// console.log(history);
 	return(
 		<div className="">
-			{[...history].reverse().map((item, i) => <div key={i} className="">{item.title}</div>)}
+			{[...history].reverse().map((item, i) => <HistoryItem key={i} className="" item={item} />)}
 		</div>
 	)
 }
