398dd3b09632b1008d4c9428b8d50af7742946e3
parunkov <parunkov@gmail.com>
2020-08-26 13:46:09 +0500

    Сделал попап

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './History.scss';
 
 const HistoryItem = ({item}) => {
+
+	const [popup, setPopup] = useState(false);
+
 	return(
 		<span className="History__item">
 			<span className={item.isError ? "History__error History__error_enabled" : "History__error"}></span>
 			<span className="History__title">{item.title}</span>
+			<button className="History__button" onClick={() => setPopup(!popup)}>...</button>
+			{popup && <div>
+				<div className="">Выполнить</div>
+				<div className="">Скопировать</div>
+				<div className="">Удалить</div>
+				</div>}
 		</span>
 	)
 }
